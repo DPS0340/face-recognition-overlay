ddbb9fd54d98af3fe8b525edfc574b1715fbefe0
jiho lee <jiholee@kpu.ac.kr>
2020-12-16 19:02:55 +0900

    Fix overlay to python 3.5

--- overlay.py
+++ overlay.py
@@ -1,19 +1,31 @@
 import time
 import cv2
 import math
-import face_detection
 from threading import Thread
-
-detector = face_detection.build_detector(
-  "DSFDDetector", confidence_threshold=.5, nms_iou_threshold=.3)
+from datetime import datetime
 
 resize_factor = 0.1
 green = (0, 255, 0)
 face_locations = []
+cascPath = "haarcascade_frontalface_alt.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+fontScale = 1
+aa = cv2.LINE_AA
+thickness = 2
+color = (0, 255, 0)
 
 def get_faces(img):
-    detections = detector.detect(img)
-    return detections
+    faces = faceCascade.detectMultiScale(
+    img
+    )
+    for face in faces:
+        face[2] += face[0]
+        face[3] += face[1]
+    if len(faces):
+        print(faces)
+    return faces
 
 
 def find_faces(image):
@@ -23,29 +35,33 @@ def find_faces(image):
     for pos in face_locations:
         for i in range(4):
             pos[i] *= 1/resize_factor
-    print(face_locations)
 
 def show_face(image):
     for pos in face_locations:
         image = process_face(image, pos)
+    image = print_now(image)
     if len(face_locations):
         image = print_welcome(image)
+    return image
+
+def print_now(image):
+    now = datetime.now()
+    timeString = now.strftime("%Y/%m/%d %H:%M:%S")
+    org = (250, 400)
+    image = cv2.putText(image, timeString, org, font, fontScale,
+                        color, thickness, aa, False)
     return image
 
 def print_welcome(image):
     org = (300, 300)
     text = "Welcome Home!"
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    fontScale = 1
-    aa = cv2.LINE_AA
-    thickness = 2
-    color = (0, 255, 0)
+
     image = cv2.putText(image, text, org, font, fontScale,
                         color, thickness, aa, False)
     return image
 
 def process_face(image, pos):
-    left, bottom, right, top, _ = pos
+    left, bottom, right, top = pos
     image = cv2.line(image, (left, bottom), (right, bottom), green, 2)
     image = cv2.line(image, (left, top), (right, top), green, 2)
     image = cv2.line(image, (left, bottom), (left, top), green, 2)
@@ -59,13 +75,10 @@ if __name__ == '__main__':
     ret,frame = cap.read() #start streaming
 
     while cap.isOpened():
-        cnt += 1
         ret, img = cap.read()
         if ret:
-            if cnt >= 5:
-                t = Thread(target=find_faces, args=(img,))
-                t.start()
-                cnt = 0
+            t = Thread(target=find_faces, args=(img,))
+            t.start()
             img = show_face(img)
             cv2.imshow("RPI 얼굴인식", img)
             if cv2.waitKey(1) & 0xFF == 27: #esc
